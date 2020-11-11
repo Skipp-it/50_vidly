f6529bcfd9bddef28e1531a0b944063ae185630c
Celalalt666 <mihairaducroitoru@gmail.com>
2020-11-12 00:29:08 +0200

    padding added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -13,34 +13,44 @@ class Movies extends Component {
     }
 
     render () {
-    return <table className="table">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            {this.state.movies.map(movie=>
-            <tr id={movie._id} key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td><button className="btn btn-danger btn-sm" onClick={()=>this.handleDelete(movie)} >Delete</button>
-                </td>
+const {length : count} = this.state.movies;
 
-            </tr>
-            )}
-        </tbody>
-    </table>
-    };
+        if (count === 0){
+            return <p>There are no movies in database.</p>
+        }
+
+    return (
+    <React.Fragment>
+    <p>Showing {count} movies from database</p>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.movies.map(movie=>
+                <tr id={movie._id} key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td><button className="btn btn-danger btn-sm" onClick={()=>this.handleDelete(movie)} >Delete</button>
+                    </td>
+                </tr>
+                )}
+            </tbody>
+        </table> 
+    </React.Fragment>
+    )};
 
 
 
+    
 }
 
 export default Movies;
